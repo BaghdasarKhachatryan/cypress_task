cd720f821795194bf8efb540d0f07c03758158e7
Baghdasar1 <Baghdasar.Khachatryan@noveogroup.com>
2022-11-27 22:20:09 +0400

    Sidenav in desktop/mobile modes

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatSidenavModule],
-  exports: [MatSidenavModule],
+  imports: [CommonModule, MatSidenavModule, MatIconModule, MatToolbarModule],
+  exports: [MatSidenavModule, MatIconModule, MatToolbarModule],
 })
 export class MaterialModule {}
---
+++ src/app/shared/ui/dananz-logo/logo.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dananz-logo',
+  templateUrl: './logo.component.svg',
+  styleUrls: ['./logo.component.scss'],
+})
+export class DananzLogoComponent {}
